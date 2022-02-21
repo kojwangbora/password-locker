1509ec7b4a749902ad9ff251e56dbe62b6c294a0
kojwangbora <kojwangbora254@gmail.com>
2022-02-21 10:55:26 +0300

    add condition statement to user and credential creation

--- run.py
+++ run.py
@@ -49,6 +49,43 @@ def generate_Password():
     auto_password=Credentials.generatePassword()
     return auto_password
 
- 
+def copy_password(account):
+    """Copies password useing pypreclip, import the framework then declare the function that copies emails"""
+    return Credentials.copy_password(account)
 
-    
+
+def passlocker():
+    print("Welcome to Password Locker..\n Please follow the instructions below to proceed.\n CR --- Create New Account \n HA --- Have An Account \n")
+    short_code = input("").lower().strip()
+    if short_code == "cr":
+        print("sign Up")
+        print('*'*50)
+        username = input("User_name: ")
+        while True:
+            print("T - To type your passcode: \n GP - Generate random Password")
+            password_Choice = input().lower().strip()
+            if password_Choice =='tp':
+                password = input("Please Enter your password\n")
+                break
+            elif password_Choice =='gp':
+                password = generate_Password()
+                break
+            else:
+                print("you have entered an invalid password, please try again")
+        save_user(create_new_user(username,password))
+        print("*"*85)
+        print(f"hello {username}, Success! Your account has been created. Your passsword is: {password}")
+        print("*"*85)
+    elif short_code == "li":
+        print("*"*50)
+        print("Please enter your username and password to login: ")
+        print('*' * 50)
+        username = input("User name: ")
+        password = input(" password: ")
+        login = login_user(username, password)
+        if login_user == login:
+            print(f"hello {username}.Welcome To Password Locker!")
+            print('\n')
+    while True:
+        print( "Use the following short codes:\n cc - Create new credentals \n dc - Display Credentials \n fc- FIind credentials \n GP - Generate a random password \n d- Delete credential \n ex - Exi application \n")
+        
