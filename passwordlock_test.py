4badad986c7f51ac6e67b47315363b92f0398a0e
kojwangbora <kojwangbora254@gmail.com>
2022-02-21 06:59:51 +0300

    add function that saves user instance into user list

--- passwordlock_test.py
+++ passwordlock_test.py
@@ -13,5 +13,20 @@ class TestClass(unittest.TestCase):
         
 
         self.new_user = User('KojwangBora', 'Babu254@')
+    
+    def test_init(self):
+        """Test case to check proper object initialization"""
+        self.assertEqual(self.new_user.username, 'KojwangBora')
+        self.assertEqual(self.new_user.password, 'Babu254@')
+    
+    def test_save_user(self):
+        """Test case to check if the new user instance in saved into the User list"""
 
+        self.new_user.save_user()
+        self.assertEqual(len(User.user_list),1)
+
+class TestCredentials(unittest, unittest.TestCase):
+    """method runs before individual credential tests runs"""
+    
+         
     
