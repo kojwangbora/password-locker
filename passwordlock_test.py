a2a981f7e9cbad8598d3dd52b524a51a21e5c1be
kojwangbora <kojwangbora254@gmail.com>
2022-02-21 06:51:02 +0300

    add function that runs before individual tests run

--- passwordlock_test.py
+++ passwordlock_test.py
@@ -3,3 +3,15 @@
 from password_lock import User
 from password_lock import Credentials
 import unittest
+
+
+class TestClass(unittest.TestCase):
+    """ Test class defining the test cases for class User."""
+
+    def setUp(self):
+        """A method that runs  before the test methods run"""
+        
+
+        self.new_user = User('KojwangBora', 'Babu254@')
+
+    
